b21af022faac2457ac0562139139c50751e2357a
JKawakami-stkn <j.kawakami.sotuken@gmail.com>
2019-09-05 22:40:29 +0900

    モーダルウィンドウを動的に生成する仕様に変更

--- resources/views/po_list.blade.php
+++ resources/views/po_list.blade.php
@@ -1,9 +1,5 @@
 @extends('./template/layout')
 <?php
-use App\models\Hannbaikai;
-use App\models\Hannbaikumi;
-use App\models\Hannbaisyouhinn;
-
 use Illuminate\Database\Eloquent\Model;
 ?>
 <!-- css -->
@@ -16,105 +12,87 @@ use Illuminate\Database\Eloquent\Model;
 @stop
 
 @section('content')
-    <!-- メインコンテンツ -->
-    <div class="main">
-        <div class="container">
-            <!-- ページ名 -->
-            <h4 class="page-title blue-text text-lighten-3">発注書一覧</h4>
+  <!-- メインコンテンツ -->
+  <div class="main">
+    <div class="container">
+      <!-- ページ名 -->
+      <h4 class="page-title blue-text text-lighten-3">発注書一覧</h4>
 
-            <!-- 新規作成ボタン -->
-            <div class="add_button">
-                <a class="btn-floating btn-large waves-effect waves-light blue" href="{{ action('PoCreateController@show') }}"><i class="material-icons">add</i></a>
+      <!-- 新規作成ボタン -->
+      <div class="add_button">
+        <a class="btn-floating btn-large waves-effect waves-light blue" href="{{ action('PoCreateController@show') }}"><i class="material-icons">add</i></a>
+      </div>
+      <p></p>
+
+
+      @foreach($hannbaikai as $hannbaikaidata)
+        <?php $hannbaikumi = $hannbaikaidata->kumis;?>
+        <!-- 発注ごと -->
+          <div class="z-depth-1 blue-text order">
+
+            <div class="row">
+              <p class="col s8 order_title">{{$hannbaikaidata->hannbaikai_name}}</p>
+
+              <div class="col s4 order_button">
+                <a class="btn-floating btn-large waves-effect waves-light green"
+                  href="{{ action('PoCheckInspectionController@show') }}" ><i class="material-icons">content_paste</i></a>
+                <a class="btn-floating btn-large waves-effect waves-light red"
+                    href="{{ action('PoPrintController@show',$hannbaikaidata->id) }}" ><i class="material-icons">local_printshop</i></a>
+              </div>
             </div>
-            <p></p>
-
-            <a id="1_1" onclick="createModal(this.id);">てすと</a>
-
-            <!--test-->
-            <script>
-                function createModal(id){
-                    console.log(id.split("_"));
-
-                }
-            </script>
-
-
-
-            @foreach($hannbaikai as $hannbaikaidata)
-                <?php $hannbaikumi = $hannbaikaidata->kumis;?>
-                <!-- 発注ごと -->
-                <div class="z-depth-1 blue-text order">
-
-                    <div class="row">
-                      <p class="col s8 order_title">{{$hannbaikaidata->hannbaikai_name}}</p>
-
-                      <div class="col s4 order_button">
-                            <a class="btn-floating btn-large waves-effect waves-light green"
-                            href="{{ action('PoCheckInspectionController@show') }}" ><i class="material-icons">content_paste</i></a>
-                            <a class="btn-floating btnz-large waves-effect waves-light red"
-                            href="{{ action('PoPrintController@show',$hannbaikaidata->id) }}" ><i class="material-icons">local_printshop</i></a>
-                        </div>
+            <!-- クラスごと -->
+            @foreach($hannbaikumi as $kumidata)
+              <span>
+                <a class="modal-trigger" href="#modal" data-hannbaikai_id="{{$hannbaikaidata->id}}" data-kumi_id="{{$kumidata->id}}" data-kumi_name="{{$kumidata->kumi_name}}">
+                <div class="card horizontal">
+                  <div class="card-stacked">
+                    <div class="card-content">
+                      <h5>{{$kumidata->kumi_name}}</h5>
                     </div>
-
-
-                        <!-- クラスごと -->
-                    @foreach($hannbaikumi as $kumidata)
-                        <span>
-                            <a class="modal-trigger" href="#{{$hannbaikaidata->hannbaikai_name}}">
-                            <div class="card horizontal">
-                              <div class="card-stacked">
-                                <div class="card-content">
-                                  <h5>{{$kumidata->kumi_name}}</h5>
-                                </div>
-                                <!--
-                                <div class="card-action">
-                                    <p>対象　:　〇〇組</p>
-                                </div>
-                                  -->
-                              </div>
-                            </div>
-                            </a>
-                        </span>
-                    @endforeach
-
+                  </div>
                 </div>
+                </a>
+              </span>
             @endforeach
 
-            <div id="{{$hannbaikaidata->hannbaikai_name}}" class="modal">
-                <div class="modal-content">
-                    <div class="modal-header center-align">
-                        <h5>発注書〇</h5>
-                    </div>
+          </div>
+      @endforeach
 
-                    <a id="fill_in" href="{{ action('PoFillInController@show') }}">
-                        <div class="row">
-                            <div class="card">
-                                <div class="card-content center-align">
-                                    <p>保護者入力</p>
-                                </div>
-                            </div>
-                        </div>
-                    </a>
+      <div id="modal" class="modal">
+        <div class="modal-content">
+          <div class="modal-header center-align">
+            <h5 id="kumi_name">発注書〇</h5>
+          </div>
 
-                    <a id="customer_list" href="{{ action('PoFillInController@show') }}">
-                        <div class="row">
-                            <div class="card">
-                                <div class="card-content center-align">
-                                    <p>注文内容確認</p>
-                                </div>
-                            </div>
-                        </div>
-                    </a>
+          <a id="fill_in" href="">
+            <div class="row">
+              <div class="card">
+                <div class="card-content center-align">
+                  <p>保護者入力</p>
+                </div>
+              </div>
+            </div>
+          </a>
 
-                    <a id="check_delivery" href="{{ action('PoFillInController@show') }}">
-                        <div class="row">
-                            <div class="card">
-                                <div class="card-content center-align">
-                                    <p>引き渡しチェック</p>
-                                </div>
-                            </div>
-                        </div>
-                    </a>
+          <a id="customer_list" href="">
+            <div class="row">
+              <div class="card">
+               <div class="card-content center-align">
+                  <p>注文内容確認</p>
+                </div>
+              </div>
+            </div>
+          </a>
+
+          <a id="check_delivery" href="">
+            <div class="row">
+              <div class="card">
+                <div class="card-content center-align">
+                  <p>引き渡しチェック</p>
+                </div>
+              </div>
+            </div>
+          </a>
 
             <!-- 削除、詳細 -->
             <!--
@@ -139,9 +117,10 @@ use Illuminate\Database\Eloquent\Model;
                   </a>
                 </div>
             -->
-            </div>
         </div>
+      </div>
     </div>
+  </div>
 
 @stop
 
@@ -156,10 +135,25 @@ use Illuminate\Database\Eloquent\Model;
     var instances = M.Collapsible.init(elems);
   });
 
-  // ポップアップ
+  // モーダル初期化
   document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.modal');
     var instances = M.Modal.init(elems);
+  });
+
+  // モーダル生成
+  $('.modal-trigger').on('click', function(){
+    var target =  $(this);
+    var hannbaikai_id = target.attr('data-hannbaikai_id');
+    var kumi_id = target.attr('data-kumi_id');
+    var kumi_name = target.attr('data-kumi_name');
+
+    // モダールタイトル書き換え
+    $('#kumi_name').text(kumi_name);
+    $("#fill_in").attr("href", "{{ action('PoFillInController@show') }}" + "/" + hannbaikai_id + "/" + kumi_id);
+    $("#customer_list").attr("href", "{{ action('PoFillInController@show') }}" + "/" + hannbaikai_id + "/" + kumi_id);
+    $("#check_delivery").attr("href", "{{ action('PoFillInController@show') }}" + "/" + hannbaikai_id + "/" + kumi_id);
+
   });
 
 </script>
