004f0f57d8e1d82497b983e07204854d035b0b8a
JKawakami-stkn <j.kawakami.sotuken@gmail.com>
2019-09-11 00:41:28 +0900

    園児の情報を取得できるようにコントローラーを修正

--- app/Http/Controllers/PoCheckDeliveryController.php
+++ app/Http/Controllers/PoCheckDeliveryController.php
@@ -4,14 +4,25 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use App\Models\purchased_articl;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class PoCheckDeliveryController extends Controller
 
 {
 
   public function show($hannbaikai_id, $kumi_id){
-      return view('po_check_delivery');
+
+    $data = DB::table('m_kids')
+      ->select('m_kids.kids_id', 'm_kids.kids_nm_kj')
+      ->leftjoin('t_kids_gp_posi', 'm_kids.kids_id', '=', 't_kids_gp_posi.kids_id')
+      ->where('t_kids_gp_posi.gp_cd', '=', $kumi_id)
+      ->get();
+
+      var_dump($data);
+
+    return view('po_check_delivery', ['data'=>$data]);
+    
   }
 
 }
