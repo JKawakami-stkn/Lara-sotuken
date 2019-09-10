b22eedca22be34ab3d70b3e2d6a8bde8aee2474b
JKawakami-stkn <j.kawakami.sotuken@gmail.com>
2019-09-11 02:51:14 +0900

    非同期通信を実装

--- app/Http/Controllers/PoCheckDeliveryController.php
+++ app/Http/Controllers/PoCheckDeliveryController.php
@@ -21,8 +21,34 @@ class PoCheckDeliveryController extends Controller
 
       var_dump($data);
 
-    return view('po_check_delivery', ['data'=>$data]);
-    
+    return view('po_check_delivery', ['data'=>$data, 'hannbaikai_id'=>$hannbaikai_id]);
+
+  }
+
+  // TODO:取引先編集画面のエラーを修正, hattyuumeisaiテーブルのtyuumonn_idを削除, hattyuumeisaiテーブルのzidou_idの型を修正
+
+  // ajax
+  public function load(Request $request, $zidou_id){
+
+    $data = DB::table('tyuumonnmeisai')
+      ->select('syouhinn.syouhinn_name', 'sku.saizu', 'sku.color')
+      ->leftjoin('sku', 'tyuumonnmeisai.sku_id', '=', 'sku.id')
+      ->leftjoin('syouhinn', 'sku.syouhinn_id', '=', 'syouhinn.id')
+      ->where('tyuumonnmeisai.zidou_id', '=', $zidou_id)
+      ->where('tyuumonnmeisai.hannbaikai_id', '=', $request->hannbaikai_id)
+      ->get()
+      ->toArray();
+      //
+
+    // jsonを返す
+    return response()->json(
+      $data,
+      200, [],
+      JSON_UNESCAPED_UNICODE
+    );
+
+    //return response()->json(['result' => true]);
+
   }
 
 }
--- resources/views/po_check_delivery.blade.php
+++ resources/views/po_check_delivery.blade.php
@@ -1,4 +1,8 @@
 @extends('./template/layout')
+
+<!-- CSRFトークン生成 -->
+<meta name="csrf-token" content="{{ csrf_token() }}">
+
 <!-- css -->
 @section('addCSS')
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
@@ -17,23 +21,13 @@
 
         <table class="products-table">
             <tr>
-                <th class="product kumi top">
-                    <div class="input-field col s12">
-                        <select>
-                          <option value=""  disabled selected> 組 </option>
-                          <option value="花組">花組</option>
-                          <option value="宙組">宙組</option>
-                          <option value="雪組">雪組</option>
-                        </select>
-                </th>
-
                 <th class="product name top">
                     <div class="input-field col s12">
-                        <select>
-                          <option value="" disabled selected> 名前 </option>
-                          <option value="真木よう子">真木よう子</option>
-                          <option value="はいだしょうこ">はいだしょうこ</option>
-                          <option value="黒木瞳">黒木瞳</option>
+                        <select id="name_selector">
+                          <option value="" disabled selected>  </option>
+                          @foreach($data as $d)
+                            <option value="{{ $d->kids_id }}"> {{ $d->kids_nm_kj }} </option>
+                          @endforeach
                         </select>
                     </div>
                 </th>
@@ -47,8 +41,7 @@
                 <tr>
                     <th class="product yohin top">用品名</th>
                     <th class="product suryo top">数量</th>
-                    <th class="product size top">サイズ</th>
-                    <th class="product box top"><h6>　　</h6></th>
+                    <th class="product box top"><h6></h6></th>
                 </tr>
             </table>
 
@@ -56,12 +49,11 @@
                 <tr>
                     <th class="product yohin top">制服</th>
                     <th class="product suryo top">1　　</th>
-                    <th class="product size top">S　　</th>
                     <th class="product box top">
                       <label>
                         <input type="checkbox" class="filled-in" checked="checked" />
                         <span></span>
-                    </label>
+                      </label>
                     </th>
                 </tr>
             </table>
@@ -72,13 +64,48 @@
 
 
 @section('addJS')
-    <script type="text/javascript" src="{{ asset('/js/materialize.min.js') }}"></script>
+  <script src="https://code.jquery.com/jquery-3.0.0.min.js"></script>
+  <script type="text/javascript" src="{{ asset('/js/materialize.min.js') }}"></script>
 
-    <!-- セレクター -->
-    <script>
-      document.addEventListener('DOMContentLoaded', function() {
-       var elems = document.querySelectorAll('select');
-       var instances = M.FormSelect.init(elems);
-      });
-    </script>
+  <!-- セレクター -->
+  <script>
+    document.addEventListener('DOMContentLoaded', function() {
+      var elems = document.querySelectorAll('select');
+      var instances = M.FormSelect.init(elems);
+    });
+  </script>
+
+  <!-- Ajax -->
+  <script>
+  $(function(){ // 遅延処理
+    $('#name_selector').change(
+      function() {
+
+          var $z_id = $('#name_selector').val();
+
+          $.ajaxSetup({
+              headers: {
+                  'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') // CSRFトークン
+              }
+          })
+          $.ajax({
+              type: 'POST',
+              url: '{{route('po_check_delivery.load')}}' + '/' + $z_id,
+              dataType: 'json', // 読み込むデータの種類
+              data: {
+                hannbaikai_id : {{$hannbaikai_id}},
+              },
+          }).done(function (results) {
+              // 成功時の処理
+              //$('#text').html(results);
+              console.log(results)
+          }).fail(function (err) {
+              // 失敗時の処理
+              alert('ファイルの取得に失敗しました。');
+          });
+      }
+    );
+  });
+  </script>
+
 @stop
